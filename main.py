c8d76053f68728c2b3e60c8ab40baccdcfbdb661
Daniel Kogan <daniel@techhacks.nyc>
2020-06-30 18:24:18 -0400

    successful

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ import os, random
 language = 'en'
 
 apology_intros = ["I made a severe and continuous lapse in my judgement, and I don’t expect to be forgiven. I’m "
-                  "simply here to apologize.", "I want to talk to you guys about some mistakes I have made",
+                  "simply here to apologize. ", "I want to talk to you guys about some mistakes I have made, ",
                   "yeah, I wasn't acting very sexy earlier...   "]
 
 bs = ["My waitress at taco bell got my order wrong three months ago", "I couldn't find my right sock this morning",
@@ -24,9 +24,8 @@ bs = ["My waitress at taco bell got my order wrong three months ago", "I couldn'
 def main():
     reason = input('Why are you apologizing? ')
     Intro = random.choice(apology_intros)
-    Middle_part = f"  I am deeply and truly sorry for {reason.lower()}. It was wrong, disgraceful, and I promise it will never happen again. {reason.lower()} is the worst thing I have ever done in my entire life, no contest. . . I don't want your forgiveness, just the open space to be able to say how deeply sorry I am for {reason.lower()} "
+    Middle_part = f".  I am deeply and truly sorry for {reason.lower()}. It was wrong, disgraceful, and I promise it will never happen again,  .   {reason.lower()} is the worst thing I have ever done in my entire life, no contest. . . I don't want your forgiveness, just the open space to be able to say how deeply sorry I am for {reason.lower()}. "
     Conclusion = f' Thank you everyone for giving me this time to apologize for my actions. It\'s just been so so hard for me since {random.choice(bs)}. I love each of you guys so so much. Thank you, and Don\'t forget to like and subscribe!'
-
     script = Intro + Middle_part + Conclusion
     print('Processing audio...')
 
@@ -37,9 +36,10 @@ def main():
     audioClip = AudioFileClip("Assets/audio.aac")
 
     MusicFile = random.choice(os.listdir('./Assets/music'))
+    #print(MusicFile)
     backgroundMusic = AudioFileClip("Assets/music/" + MusicFile)
     backgroundMusic = backgroundMusic.set_duration(audioClip.duration)
-    NewaudioClip = CompositeAudioClip([audioClip, backgroundMusic])
+    NewaudioClip = CompositeAudioClip([audioClip, backgroundMusic]).set_duration(audioClip.duration)
     print('Audio has been processed....')
 
 
@@ -51,6 +51,7 @@ def main():
     clip2 = random.choice(list_num)
     list_num.remove(clip2)
     clip3 = random.choice(list_num)
+    #print(clip1,clip2,clip3)
 
 
     clip1 = VideoFileClip("Assets/" + str(clip1) + ".mp4")
@@ -74,27 +75,6 @@ def main():
 
     final_clip.close()
     os.remove('Assets/audio.aac')
-
-    """ final_clip.set_audio(new_audioclip).write_videofile(
-        'Assets/audio.aac',
-        fps=None,
-        codec="libx264",
-        audio_codec="aac",
-        bitrate=None,
-        audio=True,
-        audio_fps=44100,
-        preset='medium',
-        audio_nbytes=4,
-        audio_bitrate=None,
-        audio_bufsize=2000,
-        # temp_audiofile="/tmp/temp.m4a",
-        # remove_temp=False,
-        # write_logfile=True,
-        rewrite_audio=True,
-        verbose=True,
-        threads=None,
-    )
-    """
 
 
 if __name__ == '__main__':
