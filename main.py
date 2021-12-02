c643c1444503dec6a53d3b1751ce242347b903dd
Daniel Kogan <54873722+daminals@users.noreply.github.com>
2021-12-02 13:46:09 -0500

    scripted

--- main.py
+++ main.py
@@ -12,22 +12,38 @@ import os, random, ffmpy, asyncio
 # TODO: Make into a website and/or discord bot -- COMPLETED DISC BOT
 # TODO: DEBUG!!! It crashes and is unable to load the video every so often, and that must be fixed
 
-language = 'en'
 
 apology_intros = ["I made a severe and continuous lapse in my judgement, and I don’t expect to be forgiven. I’m "
                   "simply here to apologize. ", "I want to talk to you guys about some mistakes I have made, ",
-                  "yeah, I wasn't acting very sexy earlier...   ", "...Long and Deep sigh..."
+                  "yeah, I wasn't acting very sexy earlier...   ", "...Long and Deep sigh...",
+                  "Hey crewmates! SORRY for being sus earlier... ", 
+                  "Hey youtube! I am so so sorry. By the way this video is sponsored by NordVPN if you wanna check it out. Use my promo code sorry fifteen for fifteen% off",
                   ]
 
 bs = ["My waitress at taco bell got my order wrong three months ago", "I couldn't find my right sock this morning",
       "My mommy took away my PS vita", "My sugar daddy stopped funding my twizzler addiction",
-      "scratched my iPhone screen", "I was tried for first degree murder", "I'm a scorpio",
+      "I scratched my iPhone screen", "I was tried for first degree murder", "I'm a scorpio",
       "I was really hungry at the time"
-      "I broke my iPhone by putting it too close to the cutting board, but it's ok my Dad will just buy me a new one"]
+      "I broke my iPhone by putting it too close to the cutting board, but it's ok my Dad will just buy me a new one",
+      "My brita filter cracked", 
+      "Mercury was in retrograde",
+      "My stock portfolio dropped two cents",
+      "Bitcoin went down",
+      ]
 
 middle = ["I don't want your forgiveness, just the open space to be able to say how deeply sorry I am.  ",
-          "The goal of my content is always to entertain, I never meant to let it get out of hand.  "
+          "The goal of my content is always to entertain, I never meant to let it get out of hand.  ",
+          "The inhumane actions that I have done were truly despicable.  ",
+          "It was once I lost one thousand insta followers that I understood the error of my ways.   "
+          "When I lost my sponsorship with Manscaped, I realized how wrong I was.  "
           ]
+
+challenge = ["It's just been so hard for me since",
+             "Life has always been difficult for me.",
+             "I don't even know what I'm doing anymore.",
+             "Life for me is like that tiktok taylor swift audio: Screaming! Crying!",
+             "You just wouldn't understand the pain of being the only funny youtuber"
+             ]
 
 def gen_ID(char):
     ID = ''
@@ -56,7 +72,7 @@ async def main(bool_inp,ID,apolo=''):
         reason = input('Why are you apologizing? ')
     Intro = random.choice(apology_intros)
     Middle_part = f".  I am deeply and truly sorry for {reason.lower()}. It was wrong, disgraceful, and I promise it will never happen again,  .   {reason.lower()} is the worst thing I have ever done in my entire life, no contest. . .  {random.choice(middle)}"
-    Conclusion = f' Thank you everyone for giving me this time to apologize for my actions. It\'s just been so so hard for me since {random.choice(bs)}. I love each of you guys so so much. Thank you, and Don\'t forget to SMASH that like button and subscribe for more content!'
+    Conclusion = f' Thank you everyone for giving me this time to apologize for my actions. {random.choice(challenge)} {random.choice(bs)}. I love each of you guys so so much. Thank you, and Don\'t forget to SMASH that like button and subscribe for more content!'
     script = Intro + Middle_part + Conclusion
     print('Processing audio...')
     
